39417373f60ee61dfe7b7ac861770db069d1dbad
rajansharma630 <rajan63072@gmail.com>
2025-12-17 14:27:12 +0000

    container config

---
+++ basic1/variable.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"   // not recommended
+let accountState;  // IF WE DON'T ASSIGN ANY VALUE IT WILL STORE AS undefined 
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
